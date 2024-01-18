3cae0bcf862f2d99fee93be373e140041e78fc2d
shamil <toshamilgis@gmail.com>
2024-01-19 01:45:22 +0300

    init

---
+++ JsonWorker/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace JsonWorker;
+
+/// <summary>
+/// Main class of the program.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    private static void Main()
+    {
+        
+    }
+}
